8f079d96b736f05468f42dda819626628bd6adfb
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2025-10-06 09:44:00 -0400

    resizable window yay

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,42 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main() {
+    int x = 1280;
+    int y = 720;
+
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
-    // Tell GLFW we want OpenGL 3.3 Core
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(x, y, "Testing", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
 
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
-        return -1;
-    }
+    gladLoadGL();
 
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(1, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
-
         glfwSwapBuffers(window);
+        
         glfwPollEvents();
     }
 
