40bfa93fefc5d349ecc0bac6c8301a7846fe802f
Ruchir-Kafle <ruchirkafle@gmail.com>
2025-11-11 14:19:08 -0500

    cube

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "../libs/shaders/shaders.h"
 
@@ -11,8 +13,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 int main() {
-    int x = 1280;
-    int y = 1280;
+    int width = 1280;
+    int height = 1280;
 
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
@@ -26,21 +28,50 @@ int main() {
 
 
     GLfloat vertices[] = {
-        // positions           // colors (RGB)
-        -0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,  // bottom left
-        0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,  // bottom right
-        0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f,  // top right
-        -0.5f,  0.5f, 0.0f,    1.0f, 1.0f, 0.0f   // top left
-    };
+        // positions           // colors (R, G, B)
+        // Front face
+        -0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, // 0 bottom-left
+        0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, // 1 bottom-right
+        0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f, // 2 top-right
+        -0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f, // 3 top-left
 
+        // Back face
+        -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f, // 4 bottom-left
+        0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f, // 5 bottom-right
+        0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f, // 6 top-right
+        -0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f  // 7 top-left
+    };
 
     GLuint indices[] = {
-        0, 1, 2,  // first triangle
-        2, 3, 0   // second triangle
+        // Front face
+        0, 1, 2,
+        2, 3, 0,
+
+        // Back face
+        5, 4, 7,
+        7, 6, 5,
+
+        // Left face
+        4, 0, 3,
+        3, 7, 4,
+
+        // Right face
+        1, 5, 6,
+        6, 2, 1,
+
+        // Top face
+        3, 2, 6,
+        6, 7, 3,
+
+        // Bottom face
+        4, 5, 1,
+        1, 0, 4
     };
 
 
-    GLFWwindow* window = glfwCreateWindow(x, y, "Testing", NULL, NULL);
+
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "Testing", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
@@ -76,25 +107,35 @@ int main() {
     // Unbinding buffers
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     Shader shaderProgram("../libs/shaders/vertexShader.vert", "../libs/shaders/fragmentShader.frag");
-    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-    GLuint timeSensitiveColor = glGetUniformLocation(shaderProgram.ID, "timeSensitiveRed");
+
+    glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shaderProgram.activateShader();
-        glUniform1f(uniID, 0.5f);
 
-        float time = glfwGetTime();
-        float redValue = sin(time) / 2.0f + 0.5f;
-        glUniform1f(timeSensitiveColor, redValue);
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f)); 
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+        int modelLocation = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLocation = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+        int projLocation = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLocation, 1, GL_FALSE, glm::value_ptr(proj));
 
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         
